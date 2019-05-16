534aedc9149aea593df2735c057219efe31dcb52
Jitka Novotná <jitka@ucw.cz>
2019-05-16 15:10:41 +0200

    funkce na vybyrani tagu

--- from-oai.py
+++ from-oai.py
@@ -7,8 +7,16 @@ verb="ListRecords"
 metadataPrefix="oai_dc"
 oai_sets=["oai_kval","oai_kvalDC"]
 # nacte 3455,411 praci
+#3892
 
-def get_oai_metadata(oai_set):
+def tag(root,tag):
+    subtree=list(r for r in root if tag in r.tag)[0]
+    if subtree is None:
+        raise "No tag "+tag
+    return subtree
+
+
+def get_oai_records(oai_set):
     url = "http://"+server+"/OAI-PUB?verb="+verb+"&metadataPrefix="+metadataPrefix+"&set="+oai_set
     def recursion(url, resumptionToken):
         if resumptionToken is None:
@@ -16,7 +24,7 @@ def get_oai_metadata(oai_set):
         else:
             response = requests.get(url+"&resumptionToken="+resumptionToken)
         root = ET.fromstring(response.text)
-        ListRecord=list(r for r in root if "ListRecords" in r.tag)[0]
+        ListRecord=tag(root,"ListRecords")
         for child in ListRecord:
             if "record" in child.tag: 
                 yield child
@@ -27,8 +35,13 @@ def get_oai_metadata(oai_set):
                 raise "Unknown tag"
     return recursion(url,None)
 
-records = get_oai_metadata(oai_sets[1])
+def hui(record):
+    metadata=tag(record,"metadata")
+    for child in metadata:
+        print(child.tag) 
+
+records = get_oai_records(oai_sets[1])
 first = list(records)[0]
 #print( ET.tostring(first) )
-
+hui(first)
 #for child in first:
