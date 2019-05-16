cdeee4f21cd4e38a84ab97ae6391a1bfab207353
Jitka Novotná <jitka@ucw.cz>
2019-05-16 16:47:20 +0200

    hotový přími import a chyba je poloviční

--- from-oai.py
+++ from-oai.py
@@ -10,9 +10,10 @@ oai_sets=["oai_kval","oai_kvalDC"]
 #3892
 
 def tag(root,tag):
-    subtree=list(r for r in root if tag in r.tag)[0]
-    if subtree is None:
-        raise "No tag "+tag
+    try:
+        subtree=list(r for r in root if tag in r.tag)[0]
+    except:
+        raise Exception("No tag {}".format(tag))
     return subtree
 
 
@@ -35,13 +36,52 @@ def get_oai_records(oai_set):
                 raise "Unknown tag"
     return recursion(url,None)
 
-def hui(record):
-    metadata=tag(record,"metadata")
-    for child in metadata:
-        print(child.tag) 
+def get_filenames(interni_id):
+    try:
+        tree = ET.parse('digital_entities/'+str(interni_id)+".xml")
+    except:
+        #print(interni_id)
+        return
+    root = tree.getroot()
+    for stream_ref in root.findall("./*stream_ref"):
+        filename = stream_ref.find('file_name').text
+        if filename != None:
+            yield filename
+    subrecords = []
+    for relations in root.findall("./*relations"):
+        for relation in relations:
+            relation_type = relation.find('type').text
+            if relation_type == "include":
+                pid = relation.find('pid').text
+                subrecords.append(pid)
+    for record in subrecords:
+        yield from get_filenames(record)
 
-records = get_oai_records(oai_sets[1])
-first = list(records)[0]
+def hui(record):
+    header=tag(record,"header")
+    identifier=tag(header,"identifier").text
+    oai_id = identifier.split(":")[-1]
+    files=list(get_filenames(oai_id))
+#    print(files)
+    for file in files:
+        print(file)
+#    if len(files) != 0:
+#        print(oai_id)
+#        metadata=tag(tag(record,"metadata"),"record")
+#        for child in metadata:
+#            print(child.tag, child.text)
+
+    
+
+records = get_oai_records(oai_sets[0])
+#first = list(records)[0]
 #print( ET.tostring(first) )
-hui(first)
-#for child in first:
+#hui(first)
+for record in records:
+    hui(record)
+records = get_oai_records(oai_sets[1])
+#first = list(records)[0]
+#print( ET.tostring(first) )
+#hui(first)
+for record in records:
+    hui(record)
