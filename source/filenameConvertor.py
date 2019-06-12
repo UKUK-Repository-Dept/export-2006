7266dcb283a7a082206e8b8fd5b5fbecd3401b39
Jitka Novotná <jitka@ucw.cz>
2019-06-12 11:36:51 +0200

    refactor haluzeni description

--- source/filenameConvertor.py
+++ source/filenameConvertor.py
@@ -1,72 +1,98 @@
-
 class FilenameConvertor:
 
-    def __init__(self):
-        pass
-    
-    def generate_description(self, files):
-        #TODO tohle celé zkonrolovat eliškou
-        bc = ['BC','bp','BP','Bakaraska','baklarka','Bc','Bakalarska','bakalarka','bakalarska']
-        dp = ['DP','Diplomova','Diplomka','diplomka','diplomova','dp','dP','dip','DIP']
-        rp = ['rigorózni','rigo','Dis','RP','phd']
-        weird = ['DT-config_guide','Dodatky','clanek','posudek']
-        vedouci = ['vedouci']
-        oponent = ['opon']
-        posudek = ['posud'] + vedouci + oponent
-        prilohy = ['Priloh','pril','Obr','příloh','grafy','summary']
-        priloha = prilohy + ['chyby','literatura']
-        classic_tags = posudek + priloha
-        def has(tags, filename):
+    side = [
+            ( ['vedouci'], "Posudek vedoucího" ),
+            ( ['opon'], "Posudek oponenta" ),
+            ( ['posud'], "Posudek" ),
+            ( ['Priloh','pril','foto','příloh','grafy','summary','config','Dodatky','clanek'], "Příloha" ),
+            ( ['chyby'], "Chyby" ),
+            ( ['literatura'], "Literatura" ),
+        ]
+    main = [
+        ( ['BC','bp','BP','Bakaraska','baklarka','Bc','Bakalarska','bakalarka','bakalarska'], "Bakalářská práce"),
+        ( ['DP','Diplomova','Diplomka','diplomka','diplomova','dp','dP','dip','DIP'], "Diplomová práce"),
+        ( ['rigorózni','rigo','Dis','RP','phd'], "Rigorózní práce"),
+        ]
+    weird = [(['DT-config_guide','Dodatky','clanek','posudek'], "TODO")]
+            
+
+    def match(self, dictionary, name):
+        matches = []
+        for tags, match in dictionary:
             for tag in tags:
-                if tag in filename:
-                    return True
-            return False
+                if tag in name:
+                    matches.append(match)
+                    break
+        if len(matches) == 0:
+            return None
+        elif len(matches) == 1:
+            return matches[0]
+        else:
+            raise Exception("More matches. {}".format(name))
+
+    def generate_description(self, files):
         if len(files) == 1:
             filename, filetype = files[0]
             if filetype == 'application/pdf':
-                if has(dp, filename) and has(rp, filename):
-                        print(filename)
-                if has(dp, filename):
-                    return [(filename, filetype, "Diplomová práce")]
-                elif has(bc, filename):
-                    return [(filename, filetype, "Bakalářská práce")]
-                elif has(rp, filename):
-                    return [(filename, filetype, "Rigorózní práce")]
-                elif has(weird, filename):
+                if self.match(self.side, filename):
                     return [] #TODO
+                    #return filename #37
                 else:
-                    return [(filename, filetype, "Závěrečná práce")]
+                    try:
+                        match = self.match(self.main, filename)
+                    except Exception as e:
+                        if "DP" in filename and ( "RP" in filename or "rigo" in filename):
+                            return [(filename, filetype, "Závěrečná práce")]
+                        else:
+                            raise e
+                    if match:
+                        return [(filename, filetype, match)]
+                    else:
+                        return [(filename, filetype, "Závěrečná práce")]
             else:
                 return [] #TODO
+                #return filename + " " + filetype #19
         else:
-            other = 0
+            side = 0
             for filename, filetype in files:
-                if not has(classic_tags,filename):
-                    other += 1
-            if other == 1:
+                try:
+                    match = self.match(self.side, filename)
+                except Exception as e:
+                    if "posudek_oponent" in filename:
+                        match = "Posudek oponenta" 
+                    elif "posudek_vedouci" in filename:
+                        match = "Posudek vedoucího"
+                    else:
+                        raise e
+                if match == None:
+                    side += 1
+            if side == 1:
                 res = []
                 for filename, filetype in files:
-                    if has(vedouci, filename):
-                        res.append((filename, filetype, "Posudek vedoucího"))
-                    elif has(oponent, filename):
-                        res.append((filename, filetype, "Posudek oponenta"))
-                    elif has(posudek, filename):
-                        res.append((filename, filetype, "Posudek"))
-                    elif has(prilohy, filename):
-                        res.append((filename, filetype, "Příloha"))
-                    elif has(['chyby'], filename):
-                        res.append((filename, filetype, "Chyby"))
-                    elif has(['literatura'], filename):
-                        res.append((filename, filetype, "Literatura"))
-                    else:
-                        if has(dp, filename):
-                            res.append((filename, filetype, "Diplomová práce"))
-                        elif has(bc, filename):
-                            res.append((filename, filetype, "Bakalářská práce"))
-                        elif has(rp, filename):
-                            res.append((filename, filetype, "Rigorózní práce"))
+                    try:
+                        match = self.match(self.side, filename)
+                    except Exception as e:
+                        if "posudek_oponent" in filename:
+                            match = "Posudek oponenta" 
+                        elif "posudek_vedouci" in filename:
+                            match = "Posudek vedoucího"
+                        #elif "DP" in filename and ( "RP" in filename or "rigo" in filename):
+                        #    match = None
                         else:
-                            res.append((filename, filetype, "Závěrečná práce"))
+                            raise e
+                    if match != None:
+                        res.append( (filename, filetype, match) )
+                    else:
+                        try:
+                            match = self.match(self.main, filename)
+                        except Exception as e:
+                            if "DP" in filename and ( "RP" in filename or "rigo" in filename):
+                                match = "Závěrečná práce"
+                            else:
+                                raise e
+                        res.append( (filename, filetype, match) )
                 return res
             else:
                 return [] #TODO
+                #return str(files)+str(side) # 57
+
